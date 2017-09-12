45a13b0617b22b93a71e6364772f734a7f8b274c
lee890720 <lee_890720@126.com>
2017-09-12 11:31:14 +0800

    arbitrageHadge v1.0

---
+++ Sources/Robots/ClosePosition/ClosePosition/ClosePosition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using cAlgo.API;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+using cAlgo.Indicators;
+
+namespace cAlgo
+{
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class ClosePosition : Robot
+    {
+        [Parameter("label")]
+        public string label { get; set; }
+
+        [Parameter("comment")]
+        public string comment { get; set; }
+
+        protected override void OnStart()
+        {
+            // Put your initialization logic here
+        }
+
+        protected override void OnTick()
+        {
+            foreach (var pos in Positions)
+            {
+                if (pos.Label == label)
+                    ClosePosition(pos);
+                if (pos.Comment == comment)
+                    ClosePosition(pos);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            // Put your deinitialization logic here
+        }
+    }
+}
